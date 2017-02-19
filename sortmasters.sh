c6761e64ce6c2f5678c4d5a51c9070a33eb945cf
Christopher Handy <handyc@metapath.org>
2017-02-19 13:32:14 -0500

    added comments

--- sortmasters.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-for i in ./unsorted.*.master.txt
-do
-cat "$i" | sort | uniq -c | sort -rn | awk '{print $1 " " $2}' > "$i".uniq.txt
-echo "Created unique sorted master for $i"
-done
-echo "Completed sorting of masters."
